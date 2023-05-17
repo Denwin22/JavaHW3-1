9a5797db2c611655f77eb92185fc1e88addbda11
Denwin <biscooter@mail.ru>
2023-05-17 14:50:45 +0600

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+        public int calculate(int ticketPrice) {
+            int milesPerRub = 20;
+            int miles = ticketPrice / milesPerRub;
+
+            return miles;
+        }
+
+}
